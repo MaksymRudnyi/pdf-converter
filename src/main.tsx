9ddf54a07a5c536b9c7317b76eb68531c04836b8
Maksym Rudnyi <max.prog.zz@gmail.com>
2025-05-02 19:04:47 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.tsx'
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <p className={"text-3xl font-bold underline"}>test</p>
+  </StrictMode>,
+);
